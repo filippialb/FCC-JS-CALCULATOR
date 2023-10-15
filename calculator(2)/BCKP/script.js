63d6e93a7b12a40f018e6d4be2f1a62cbddd556f
Albuquerque <filippialbuquerque@gmail.com>
2023-10-15 09:29:40 -0300

    other calculator

---
+++ calculator(2)/BCKP/script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+  let display = document.getElementById('display');
+  let buttons = Array.from(document.querySelectorAll('button'));
+
+  buttons.map(button => {
+    button.addEventListener('click', function () {
+      if (button.innerText === 'C') {
+        display.innerText = '0';
+      } else if (button.innerText === '=') {
+        try {
+          display.innerText = eval(display.innerText);
+        } catch {
+          display.innerText = 'Error';
+        }
+      } else {
+        if (display.innerText === '0' && button.innerText !== '.') {
+          display.innerText = button.innerText;
+        } else if (display.innerText !== '0' || button.innerText === '.') {
+          display.innerText += button.innerText;
+        }
+      }
+    });
+  });
+});
